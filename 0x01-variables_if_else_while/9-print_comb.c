8d59743b9ac174126f95be9001916eeeb2261a00
“UncleTed01” <godswilokoyo@gmail.com>
2023-07-14 14:04:01 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,54 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints all possible single-digit numbers
+ * in ascending order, separated by comma and space.
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	int i;
+	putchar('0');
+	putchar(',');
+	putchar(' ');
 
-	for (i = '0'; i <= '9'; i++)
-	{
-		putchar(i);
-		if (i != '9')
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
+	putchar('1');
+	putchar(',');
+	putchar(' ');
 
+	putchar('2');
+	putchar(',');
+	putchar(' ');
+
+	putchar('3');
+	putchar(',');
+	putchar(' ');
+
+	putchar('4');
+	putchar(',');
+	putchar(' ');
+
+	putchar('5');
+	putchar(',');
+	putchar(' ');
+
+	putchar('6');
+	putchar(',');
+	putchar(' ');
+
+	putchar('7');
+	putchar(',');
+	putchar(' ');
+
+	putchar('8');
+	putchar(',');
+	putchar(' ');
+
+	putchar('9');
 	putchar('\n');
 
-	return 0;
+	return (0);
 }
 
