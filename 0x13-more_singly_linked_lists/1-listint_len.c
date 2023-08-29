abeee1f0cdc988bc890ccfbd9f52a555890bc5d0
“UncleTed01” <godswilokoyo@gmail.com>
2023-08-29 16:57:04 -0700

    singly limked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
- *
- * Return: number of nodes
- */
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: pointer to the list.
+ * Return: number of nodes.
+ **/
 size_t listint_len(const listint_t *h)
 {
-	size_t num = 0;
+	const listint_t *node = h;
+	size_t cont = 0;
 
-	while (h)
+	while (node)
 	{
-		num++;
-		h = h->next;
+		cont++;
+		node = node->next;
 	}
 
-	return (num);
+	return (cont);
 }
